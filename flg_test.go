99e9d20ca7ab23304dd7013db8eb06be5680251a
bikang1 <bikang1@staff.weibo.com>
2022-02-08 22:45:00 +0800

    go

--- flg_test.go
+++ flg_test.go
@@ -2,46 +2,41 @@ package flg
 
 import (
 	"testing"
-	"go.uber.org/zap"
+
 	"github.com/BurntSushi/toml"
+	"go.uber.org/zap"
 )
 
-
-var(
-
+var (
 	lg *zap.Logger
 )
 
 /**
- 测试初始化
- */
-func TestLoadFromFile(t *testing.T){
+测试初始化
+*/
+func TestLoadFromFile(t *testing.T) {
 	l := &Logger{}
-	err := l.LoadFromFile("test.toml")
+	err := l.LoadFromFile("./data/test.toml")
 	if err != nil {
 		panic("get file faild")
 	}
-	lg,err := l.GetLogByKey("test")
+	lg, err := l.GetLogByKey("test")
 	lg.Info("a test")
 
-	lg.Info("abc",zap.Int("int",11))
+	lg.Info("abc", zap.Int("int", 11))
 }
 
-func TestLoadFromObject(t *testing.T){
+func TestLoadFromObject(t *testing.T) {
 	var fconfig FConfig
 	l := &Logger{}
-	if _, err := toml.DecodeFile("test.toml", &fconfig); err != nil {
+	if _, err := toml.DecodeFile("./data/test.toml", &fconfig); err != nil {
 		panic(err)
 	}
 	err := l.LoadFromObject(&fconfig)
 	if err != nil {
 		panic("TestLoadFromObject faild")
 	}
-	lg,err := l.GetLogByKey("test")
+	lg, err := l.GetLogByKey("test")
 	lg.Info("a test")
-	lg.Info("abc",zap.Int("int",11))
+	lg.Info("abc", zap.Int("int", 11))
 }
-
-
-
-
--- go.mod
+++ go.mod
@@ -1,13 +1,9 @@
-module github.com/beckbikang/flg
+module flg
 
-go 1.12
+go 1.16
 
 require (
-	github.com/BurntSushi/toml v0.3.1
-	github.com/pkg/errors v0.8.1 // indirect
-	github.com/stretchr/testify v1.4.0 // indirect
-	go.uber.org/atomic v1.4.0 // indirect
-	go.uber.org/multierr v1.1.0 // indirect
-	go.uber.org/zap v1.10.0
+	github.com/BurntSushi/toml v1.0.0
+	go.uber.org/zap v1.20.0
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0
 )
